a848686cef9d7b9cd462d2cb207be81b19b4d5df
dankogai <dankogai@d0d07461-0603-4401-acd4-de1884942a52>
2011-12-18 23:24:59 +0000

    modernized and optimized
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/math-complex/trunk@39085 d0d07461-0603-4401-acd4-de1884942a52

--- complex.js
+++ complex.js
@@ -1,14 +1,21 @@
 /*
- * $Id: complex.js,v 0.6 2010/08/13 21:39:40 dankogai Exp dankogai $
+ * $Id: complex.js,v 0.7 2011/12/18 23:22:13 dankogai Exp dankogai $
  */
 
-(function(){
-    if (Math.Complex) return;
-    Math.Complex = function(re, im){
-       this.re = re ? re : 0;
-       this.im = im ? im : 0;
+(function(global){
+    if (global.Math.Complex) return;
+    Math.Complex = function Complex(re, im){
+	if (this instanceof Math.Complex){
+	    this.re = 0 + re;
+	    this.im = 0 + im;
+	}else{
+	    return new Math.Complex(re, im);
+	}
     };
-    Math.Complex.prototype = {
+    var CPLX = Math.Complex;
+    (function(methods){
+	for (var p in methods) CPLX.prototype[p] = methods[p];
+    })({
         toString:function(){
             var s =  '' + this.re;
             if (this.im) {
@@ -19,45 +26,45 @@
             return s;
         },
         neg:function(){
-          return new Math.Complex(-this.re,-this.im);
+          return new CPLX(-this.re,-this.im);
         },
         con:function(){
-          return new Math.Complex(this.re,-this.im);
+          return new CPLX(this.re,-this.im);
         },
         arg:function(){
           return Math.atan2(this.im, this.re);
         },
         abs:function(){
           return Math.sqrt(this.re*this.re + this.im*this.im);
-        },        
+        },
         add:function(that){
             return (that.constructor === this.constructor)
-                ? new Math.Complex(this.re + that.re, this.im + that.im)
-                : new Math.Complex(this.re + that*1,  this.im);
+                ? new CPLX(this.re + that.re, this.im + that.im)
+                : new CPLX(this.re + that*1,  this.im);
         },
         sub:function(that){
             return (that.constructor === this.constructor)
-                ? new Math.Complex(this.re - that.re, this.im - that.im)
-                : new Math.Complex(this.re - that*1,  this.im);
+                ? new CPLX(this.re - that.re, this.im - that.im)
+                : new CPLX(this.re - that*1,  this.im);
         },
         mul:function(that){
             return (that.constructor === this.constructor)
-                ? new Math.Complex(
+                ? new CPLX(
                     this.re * that.re - this.im * that.im, 
                     this.im * that.re + this.re * that.im
                 )
-                : new Math.Complex(this.re * (that*1),  this.im * (that*1));
+                : new CPLX(this.re * (that*1),  this.im * (that*1));
         },
         div:function(that){
             if (that.constructor === this.constructor){
                 var d = that.re * that.re + that.im * that.im;
-                if (d === 0) return new Math.Complex(this.re/0, this.im/0);
-                return new Math.Complex(
+                if (d === 0) return new CPLX(this.re/0, this.im/0);
+                return new CPLX(
                     (this.re * that.re + this.im * that.im) / d, 
                     (this.im * that.re - this.re * that.im) / d
                 );
             }else{
-                return new Math.Complex(
+                return new CPLX(
                     this.re / (that*1),  this.im / (that*1)
                 );
             }
@@ -65,10 +72,10 @@
         exp:function(){
             var abs = Math.exp(this.re);
             var arg = this.im;
-            return new Math.Complex(abs*Math.cos(arg), abs*Math.sin(arg));
+            return new CPLX(abs*Math.cos(arg), abs*Math.sin(arg));
         },
         log:function(){
-            return new Math.Complex(
+            return new CPLX(
                 Math.log(this.abs()),
                 this.arg()
             );
@@ -77,10 +84,10 @@
             if (that.constructor === this.constructor){
                 return that.mul(this.log()).exp();                
             }else{
-                if (this.re < 0) return this.pow(new Math.Complex(that, 0));
+                if (this.re < 0) return this.pow(new CPLX(that, 0));
                 var abs = Math.pow(this.abs(), that*1);
                 var arg = this.arg() * that*1;
-                return new Math.Complex(
+                return new CPLX(
                     abs * Math.cos(arg),
                     abs * Math.sin(arg)
                 );
@@ -90,16 +97,16 @@
             if (that.constructor === this.constructor){
                 return this.re === that.re && this.im === that.im;
             }else{
-                return this.eq(new Math.Complex(that, 0));
+                return this.eq(new CPLX(that, 0));
             }
         },
-    ne:function(that){
-        return ! this.eq(that);
-    }
-    };
+	ne:function(that){
+	    return ! this.eq(that);
+	    }
+    });
     /* functions exported for convenience */
-    cplx  = function(re, im){ return new Math.Complex(re, im) };
-    cplxe = function(abs, arg){
-      return new Math.Complex(abs*Math.cos(arg), abs*Math.sin(arg));
+    global.cplx  = function(re, im){ return new CPLX(re, im) };
+    global.cplxe = function(abs, arg){
+	return new CPLX(abs*Math.cos(arg), abs*Math.sin(arg));
     }
-})();
+})(this);
